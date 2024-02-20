764b65964bc92ae19041d47a349e8a8268c5b1a8
SeokHoon <sughoon1@gmail.com>
2024-02-20 23:51:13 +0900

    11

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const url1=`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
 const url2="https://musical-strudel-f7ea63.netlify.app//top-headlines";
 let news =[];
 const getLatestNews= async ()=>{
-  const url = new URL(url2);
-  console.log("uuu:",url);
+  const requestUrl = new URL(url2);
+  console.log("uuu:",requestUrl);
 
-  const response = await fetch(url);
+  const response = await fetch(requestUrl);
   const data = await response.json();
   news = data.articles;
   console.log("rrr",response);
