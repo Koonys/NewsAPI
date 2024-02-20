9f51fd4a7165c80802f6aa63247a51dfcdef5fd4
SeokHoon <sughoon1@gmail.com>
2024-02-20 23:40:59 +0900

    oo

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-let names = [
-    "Steven Paul Jobs",
-    "Bill Gates",
-    "Mark Elliot Zuckerberg",
-    "Elon Musk",
-    "Jeff Bezos",
-    "Warren Edward Buffett",
-    "Larry Page",
-    "Larry Ellison",
-    "Tim Cook",
-    "Lloyd Blankfein",
-  ];
+const API_KEY = `4564c48784eb454390f0f4dcd82383e9`
+const url1=`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+const url2="http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines";
+let news =[];
+const getLatestNews= async ()=>{
+  const url = new URL(url2);
+  console.log("uuu:",url);
 
-let someName = names.some(name => {
-    let spName = name.split(" ");
-    spName.pop();
-    return spName.some(item=>item.toLowerCase().includes("p"));
-})
-
-console.log(someName);
+  const response = await fetch(url);
+  const data = await response.json();
+  news = data.articles;
+  console.log("rrr",response);
+  console.log("ddd",data);
+  console.log("aaa",news);
+};
+getLatestNews();
